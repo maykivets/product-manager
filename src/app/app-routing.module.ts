f5de58e0698799f3cb16baf1fcb6bf024890cc44
O.Maykivets <o.maykivets@viseven.com>
2019-08-17 14:05:51 +0300

    add auth guard, login, signup, reset password

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,24 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
+import { AuthGuard } from './services/user/auth.guard';
+
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
-  { path: 'product-create', loadChildren: './pages/product-create/product-create.module#ProductCreatePageModule' },
+  {
+    path: 'product-create',
+    loadChildren: './pages/product-create/product-create.module#ProductCreatePageModule',
+    canActivate: [AuthGuard],
+  },
   { path: 'product-detail/:id', loadChildren: './pages/product-detail/product-detail.module#ProductDetailPageModule' },
   { path: 'product-list', loadChildren: './pages/product-list/product-list.module#ProductListPageModule' },
   { path: 'login', loadChildren: './pages/login/login.module#LoginPageModule' },
-  { path: 'profile', loadChildren: './pages/profile/profile.module#ProfilePageModule' },
+  {
+    path: 'profile',
+    loadChildren: './pages/profile/profile.module#ProfilePageModule',
+    canActivate: [AuthGuard]
+  },
   { path: 'reset-password', loadChildren: './pages/reset-password/reset-password.module#ResetPasswordPageModule' },
   { path: 'signup', loadChildren: './pages/signup/signup.module#SignupPageModule' },
 ];
--- src/app/services/user/auth.service.ts
+++ src/app/services/user/auth.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@angular/core';
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +9,33 @@ import { Injectable } from '@angular/core';
 export class AuthService {
 
   constructor() { }
+
+  loginUser(email: string, password: string): Promise<firebase.auth.UserCredential> {
+    return firebase.auth().signInWithEmailAndPassword(email, password);
+  }
+
+  signupUser(email: string, password: string): Promise<any> {
+    return firebase
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
+      .then((newUserCredential: firebase.auth.UserCredential) => {
+        firebase
+          .firestore()
+          .doc(`/userProfile/${newUserCredential.user.uid}`)
+          .set({ email })
+          .then((response) => console.log('setting Email operation response:', response));
+      })
+      .catch(error => {
+        console.error(error);
+        throw new Error(error);
+      });
+  }
+
+  resetPassword(email: string): Promise<void> {
+    return firebase.auth().sendPasswordResetEmail(email);
+  }
+
+  logoutUser(): Promise<void> {
+    return firebase.auth().signOut();
+  }
 }
